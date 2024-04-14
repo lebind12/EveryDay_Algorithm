9207fa367dfc3943240e32e12cfb8e0be9be638b
이우진 <52599002+lebind12@users.noreply.github.com>
2024-04-15 05:03:59 +0900

    [Gold IV] Title: 최단경로, Time: 624 ms, Memory: 141976 KB -BaekjoonHub

---
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -0,0 +1,127 @@
+# import sys
+# import heapq
+
+
+# input = sys.stdin.readline
+
+# V, E = map(int, input().rstrip().split(' '))
+# start = int(input())
+# INF = int(1e9)
+# dp = [INF] * (V+1)
+# visited = [False] * (V+1)
+# graph = [[] for _ in range(V+1)]
+# for _ in range(E):
+#     u, v, w = map(int, input().rstrip().split(' '))
+#     graph[u].append([v, w])
+# # print(graph)
+# def get_smallest_node():
+#     index = -1
+#     length = INF
+#     for i in range(len(dp)):
+#         if dp[i] < length and not visited[i]:
+#             length = dp[i]
+#             index = i
+#     return index
+
+# def dijkstra(start):
+#     visited[start] = True
+#     for end, weight in graph[start]:
+#         dp[end] = min(dp[end], weight)
+#     for i in range(V-1):
+#         now = get_smallest_node()
+#         visited[now] = True
+#         for end, weight in graph[now]:
+#             cost = dp[now] + weight
+#             dp[end] = min(dp[end], cost)
+            
+# def Dijkstra(start):
+#     #가중치 테이블에서 시작 정점에 해당하는 가중치는 0으로 초기화
+#     dp[start] = 0
+#     heapq.heappush(heap,(0, start))
+
+#     #힙에 원소가 없을 때 까지 반복.
+#     while heap:
+#         wei, now = heapq.heappop(heap)
+
+#         #현재 테이블과 비교하여 불필요한(더 가중치가 큰) 튜플이면 무시.
+#         if dp[now] < wei:
+#             continue
+
+#         for w, next_node in graph[now]:
+#             #현재 정점 까지의 가중치 wei + 현재 정점에서 다음 정점(next_node)까지의 가중치 W
+#             # = 다음 노드까지의 가중치(next_wei)
+#             next_wei = w + wei
+#             #다음 노드까지의 가중치(next_wei)가 현재 기록된 값 보다 작으면 조건 성립.
+#             if next_wei < dp[next_node]:
+#                 #계산했던 next_wei를 가중치 테이블에 업데이트.
+#                 dp[next_node] = next_wei
+#                 #다음 점 까지의 가증치와 다음 점에 대한 정보를 튜플로 묶어 최소 힙에 삽입.
+#                 heapq.heappush(heap,(next_wei,next_node))
+    
+# # print(dp)
+# dijkstra(start)
+# answer = []
+# for i in range(1, len(dp)):
+#     if i == start:
+#         answer.append("0")
+#     elif dp[i] == INF:
+#         answer.append("INF")
+#     else:
+#         answer.append(str(dp[i]))
+# print("\n".join(answer))
+
+import sys
+import heapq
+
+input = sys.stdin.readline
+INF = sys.maxsize
+V, E = map(int, input().split())
+#시작점 K
+K = int(input())
+#가중치 테이블 dp
+dp = [INF]*(V+1)
+heap = []
+graph = [[] for _ in range(V + 1)]
+
+# def Dijkstra(start):
+#     #가중치 테이블에서 시작 정점에 해당하는 가중치는 0으로 초기화
+#     dp[start] = 0
+#     heapq.heappush(heap,(0, start))
+
+#     #힙에 원소가 없을 때 까지 반복.
+#     while heap:
+#         wei, now = heapq.heappop(heap)
+#         if dp[now] < wei:
+#             continue
+
+#         for w, next_node in graph[now]:
+#             next_wei = w + wei
+#             if next_wei < dp[next_node]:
+#                 dp[next_node] = next_wei
+#                 heapq.heappush(heap,(next_wei,next_node))
+
+def Dijkstra(start):
+    dp[start] = 0
+    heapq.heappush(heap, (0, start))
+    
+    while heap:
+        weight, now = heapq.heappop(heap)
+        if dp[now] < weight:
+            continue
+        for w, next in graph[now]:
+            cost = w + weight
+            if cost < dp[next]:
+                dp[next] = cost
+                heapq.heappush(heap, (cost, next))
+        
+        
+#초기화
+for _ in range(E):
+    u, v, w = map(int, input().split())
+    #(가중치, 목적지 노드) 형태로 저장
+    graph[u].append((w, v))
+
+
+Dijkstra(K)
+for i in range(1,V+1):
+    print("INF" if dp[i] == INF else dp[i])
