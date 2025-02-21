4d2f6a1232171edb7e9c3343f5ea6d070f17b85b
이우진 <52599002+lebind12@users.noreply.github.com>
2025-02-21 19:58:45 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 2808 ms, Memory: 282012 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
@@ -0,0 +1,30 @@
+def find(parent, x):
+    if parent[x] == x:
+        return x
+    parent[x] = find(parent, parent[x])
+    return parent[x]
+
+def union(parent, a, b):
+    rootA = find(parent, a)
+    rootB = find(parent, b)
+
+    if a == b:
+        return
+    if rootA < rootB:
+        parent[rootB] = rootA
+    else:
+        parent[rootA] = rootB
+
+V, E = map(int, input().split(' '))
+array = []
+answer = []
+parent = [i for i in range(V+1)]
+for _ in range(E):
+    array.append(list(map(int, input().split(' '))))
+array.sort(key=lambda x: x[2])
+
+for a, b, cost in array:
+    if find(parent, a) != find(parent, b):
+        union(parent, a, b)
+        answer.append(cost)
+print(sum(answer) - max(answer))
