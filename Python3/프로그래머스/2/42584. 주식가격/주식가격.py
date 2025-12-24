5f79ae95c3d989180156961bfd5af8b480a86525
이우진 <52599002+lebind12@users.noreply.github.com>
2025-12-24 14:21:54 +0900

    [level 2] Title: 주식가격, Time: 34.57 ms, Memory: 19.2 MB -BaekjoonHub

---
+++ Python3/프로그래머스/2/42584. 주식가격/주식가격.py
@@ -0,0 +1,11 @@
+def solution(prices):
+    length = len(prices)
+    answer = [ i for i in range (length - 1, -1, -1)]
+    
+    stack = [0]
+    for i in range (1, length, 1):
+        while stack and prices[stack[-1]] > prices[i]:
+            j = stack.pop()
+            answer[j] = i - j
+        stack.append(i)
+    return answer
