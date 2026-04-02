d3e57acc5ad4f4beedee651871d45ad0a5a8de93
이우진 <52599002+lebind12@users.noreply.github.com>
2026-04-02 20:49:01 +0900

    [Silver III] Title: 피보나치는 지겨웡~, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ PyPy3/백준/Silver/17175. 피보나치는 지겨웡～/피보나치는 지겨웡～.py
@@ -0,0 +1,14 @@
+import sys
+
+input = sys.stdin.readline
+n = int(input())
+
+dp = [0] * 51
+
+dp[0] = 1
+dp[1] = 1
+
+for i in range(2, n+1):
+    dp[i] = dp[i-1] + dp[i-2] + 1
+
+print(dp[n] % 1_000_000_007)
